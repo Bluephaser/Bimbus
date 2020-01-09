8fb7aa95100768ee1dda065c7d6454eb63c2f56b
Noah Cobb <Noah.Cobb@DigiPen.edu>
2020-01-09 14:27:54 -0800

    Tracking

---
+++ Arcade Game/Assets/Scripts/EnemyTracking.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTracking : MonoBehaviour
+{
+    public GameObject Target;
+    public int EnemySpeed;
+    public int RotationSpeed;
+    Rigidbody2D myRB;
+    public float ChaseSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Target = FindObjectOfType<PlayerController>().gameObject; 
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Target != null)
+        {
+            Vector3 toTarget = Target.transform.position - transform.position;
+
+            Track(toTarget);
+        }
+    }
+    void Track(Vector3 toTarget)
+    {
+        myRB.velocity = toTarget.normalized * ChaseSpeed;
+    }
+}
