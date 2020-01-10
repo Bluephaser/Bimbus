68773f2f25cad60b40104e546ab690c77f9c3cff
Noah Cobb <Noah.Cobb@DigiPen.edu>
2020-01-10 13:14:49 -0800

    Score Script

---
+++ Arcade Game/Assets/Scripts/PointSystem.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PointSystem : MonoBehaviour
+{
+    float timer = 0;
+    private int currentScore;
+    public Text ScoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScore = 0;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleScore();
+        timer += Time.deltaTime;
+        
+        if(timer >= 5)
+        {
+            ++currentScore;
+            timer = 0;
+        }
+    }
+    private void HandleScore()
+    {
+        ScoreText.text = "Player Score: " + currentScore;
+    }
+}
