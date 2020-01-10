9bc967eb631d76aa5186c9d0bbbf94b9681d2d54
Bluephaser <liam.binford@digipen.edu>
2020-01-10 12:57:25 -0800

    Made PlayerController.cs

--- Arcade Game/Assets/Scripts/PlayerController.cs
+++ Arcade Game/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿/*
+ PlayerController.cs
+ By: Liam Binford
+ Last Edited: 1/10/20
+ Description: Script to allow the play to control their character with slidy, physics-based movement
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public int speed = 5;
+    Rigidbody2D myRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = new Vector2(0, 0);
+        //check for forward and backward movement
+        if(Input.GetAxis("Vertical") > 0.1)
+        {
+            movement += speed * new Vector2(0, transform.up.y).normalized;
+        }
+        if(Input.GetAxis("Vertical") < -0.1)
+        {
+            movement -= speed * new Vector2(0, transform.up.y).normalized;
+        }
+        //check for left and right movement
+        if (Input.GetAxis("Horizontal") > 0.1)
+        {
+            movement += speed * new Vector2(transform.up.x, 0).normalized;
+        }
+        if (Input.GetAxis("Horizontal") < -0.1)
+        {
+            movement -= speed * new Vector2(transform.up.x, 0).normalized;
+        }
+
+        //add force based on the prior inputs
+        myRB.AddForce(movement * Time.deltaTime);
     }
 }
